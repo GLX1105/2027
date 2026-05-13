68ef680d7bde52105460adb7321b31ca8241c3ef
GLX1105 <562761508@qq.com>
2026-05-13 15:10:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+// 网站首页
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// 测试接口
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Railway后端正常运行' });
 });
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`运行在端口 ${port}`);
+});
